c57f9b4ada5d2dc4d7a4b1dad9e48ca8cca4769a
shazzzyyy <saeedshahzajb@gmail.com>
2026-04-12 05:19:56 +0500

    Remove old project files, keep student portal frontend only

--- backend/src/main/java/com/flex/student/repository/AcademicCalendarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.flex.student.repository;
-
-import com.flex.student.model.AcademicCalendar;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AcademicCalendarRepository extends JpaRepository<AcademicCalendar, Long> {
-}
